49974d05d063ba6b7d3614634c293b69966ed77f
MarkusSteinbrecher <m.steinbrecher@swiss-property.ch>
2023-04-10 19:14:32 +0200

    Mrks

--- src/script.js
+++ src/script.js
@@ -1,28 +1,118 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui';
+
+
+/**
+ * Debug
+ */
+const gui = new GUI()
+
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+/**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const mrksTexture = textureLoader.load('mrks.jpeg')
+
+
+/**
+ * Object materials
+ */
+
+const mrksMaterial = new THREE.MeshBasicMaterial()
+mrksMaterial.map = mrksTexture
+mrksMaterial.color = new THREE.Color(0xfce205)
+mrksMaterial.shiniess = 100
+
+
+const mrks = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 23, 42),
+    mrksMaterial
+)
+
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
 
 // Scene
 const scene = new THREE.Scene()
+scene.add(mrks, pointLight)
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
-    width: 800,
-    height: 600
-}
+    width: window.innerWidth,
+    height: window.innerHeight
+} 
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 5
+camera.position.y = 5
+camera.position.z = 5
 scene.add(camera)
 
-// Renderer
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Rotate mrks
+    mrks.rotation.y = .23 * elapsedTime
+    mrks.rotation.x = .42 * elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
